2d5828e1bcefb397f7d6b988c05392dbc2ff6237
Sumit Paul <sumitpaul16102002@gmail.com>
2022-06-02 17:41:19 +0530

    v1

--- src/components/button/DOBbuttonDark.tsx
+++ src/components/button/DOBbuttonDark.tsx
@@ -16,19 +16,19 @@ export const DOBbuttonDark: FC<IProps> = (props) => {
   return (
     <Button
       onClick={props.onClick}
-      className="rounded-md p-3 button-text-lower text-white bg-[#121212] hover:bg-[#121212]"
+      className="rounded-md py-3 px-4 button-text-lower text-white bg-[#121212] hover:bg-[#121212]"
       sx={{
         '.MuiTouchRipple-child': {
           backgroundColor: 'rgba(225, 225, 255, 0.2) !important',
         },
       }}
     >
-      <div className="flex space-x-2">
+      <div className="flex space-x-3">
         <Image
           height={20}
           width={20}
-          className="opacity-50"
-          src="/icons/balloon.svg"
+          className="opacity-70"
+          src="/icons/candle.svg"
           alt="balloon icon"
         />
         <h6 className="text-white text-[13px] font-normal">{props.label}</h6>
