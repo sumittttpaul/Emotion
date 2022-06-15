d42c48d30f2609b4384e5efb25b49be3fd43f428
Sumit Paul <sumitpaul16102002@gmail.com>
2022-06-15 20:21:55 +0530

    v1

--- src/components/footer/assets/PageFooterSubscribe.tsx
+++ src/components/footer/assets/PageFooterSubscribe.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState, ChangeEvent, KeyboardEvent } from 'react';
 import { SubscribeButton } from '../../button/SubscribeButton';
 
 interface IProps {}
@@ -8,10 +8,42 @@ interface IProps {}
  * @function @PageFooterSubscribe
  **/
 
+var emailExpression =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export const PageFooterSubscribe: FC<IProps> = (props) => {
+  const [Disabled, setDisabled] = useState(true);
+  const [Email, setEmail] = useState('');
+  const [ValidEmail, setValidEmail] = useState(false);
+
+  const EmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  var ValidateEmail = Email.toLowerCase().match(emailExpression);
+
+  const EmailKeyUp = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (ValidateEmail) {
+      setDisabled(false);
+      setValidEmail(true);
+    }
+  };
   return (
-    <div className="overscroll-none">
-      <SubscribeButton />
+    <div className="overscroll-none h-full flex items-start relative">
+      <div className="flex relative bg-[#121212] rounded-lg p-0">
+        <input
+          value={Email}
+          onChange={EmailChange}
+          onKeyUp={EmailKeyUp}
+          placeholder="Email Address"
+          type="text"
+          className="text-[13px] whitespace-nowrap rounded-l-lg w-[220px] p-[10px] font-normal text-[rgba(255,255,255,0.9)] bg-[#121212] border-0 ring-0 outline-none"
+        />
+        <SubscribeButton
+          validEmail={ValidEmail}
+          disabled={Disabled}
+        />
+      </div>
     </div>
   );
 };
