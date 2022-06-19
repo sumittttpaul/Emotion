5e8a077fdd6ed0325d3ad247181382b73dce370e
Sumit Paul <sumitpaul16102002@gmail.com>
2022-06-19 11:12:44 +0530

    v1

--- src/components/footer/PageFooter/assets/ScrollToTopButton.tsx
+++ src/components/footer/PageFooter/assets/ScrollToTopButton.tsx
@@ -1,6 +1,7 @@
 import { ChevronUpIcon } from '@heroicons/react/solid';
 import { IconButton } from '@mui/material';
 import React, { Component } from 'react';
+import { TooltipDark } from '../../../tooltip/TooltipDark';
 
 interface IProps {}
 
@@ -29,21 +30,27 @@ export class ScrollToTopButton extends Component<IProps> {
   render() {
     return (
       <div className="absolute top-0 right-0 p-2.5 md-900:p-8">
-        <IconButton
-          id={ScrollButtonID}
-          disableFocusRipple
-          aria-label="scroll-to-top-button"
-          className="block opacity-80 header-button-hover transition-all duration-300 button-text-lower h-full p-2 border border-solid border-[rgba(255,255,255,0.23)]"
-          sx={{
-            borderRadius: '6px !important',
-            '.MuiTouchRipple-child': {
-              borderRadius: '0 !important',
-              backgroundColor: 'rgba(225, 225, 255, 0.5) !important',
-            },
-          }}
+        <TooltipDark
+          arrow
+          placement="bottom"
+          title={<h6 className="font-[400]">Back to Top</h6>}
         >
-          <ChevronUpIcon className="h-5 w-5 opacity-80 header-icon-hover text-white" />
-        </IconButton>
+          <IconButton
+            id={ScrollButtonID}
+            disableFocusRipple
+            aria-label="scroll-to-top-button"
+            className="block opacity-80 header-button-hover transition-all duration-300 button-text-lower h-full p-2 border border-solid border-[rgba(255,255,255,0.23)]"
+            sx={{
+              borderRadius: '6px !important',
+              '.MuiTouchRipple-child': {
+                borderRadius: '0 !important',
+                backgroundColor: 'rgba(225, 225, 255, 0.5) !important',
+              },
+            }}
+          >
+            <ChevronUpIcon className="h-5 w-5 opacity-80 header-icon-hover text-white" />
+          </IconButton>
+        </TooltipDark>
       </div>
     );
   }
