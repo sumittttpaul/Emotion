5deac72859a9fc205992bc27364cd360a3e2f2bd
Sumit Paul <sumitpaul16102002@gmail.com>
2022-06-26 12:02:29 +0530

    v1

--- src/components/banner/DiscoverBanner.tsx
+++ src/components/banner/DiscoverBanner.tsx
@@ -17,7 +17,7 @@ export const DiscoverBanner: FC<IProps> = (props) => {
     <div className="flex w-full overflow-x-hidden overflow-y-visible mt-[50px]">
       <div className="flex relative box-border space-x-5 md-900:space-x-6 p-5">
         {props.ContentArray.map((value, index) => (
-          <div key={index} className="text-white button-text-lower p-0 m-0 rounded-2xl overhidden bg-opacity-5 bg-white">
+          <div key={index} className="text-white relative button-text-lower p-0 m-0 rounded-2xl overhidden bg-opacity-5 bg-white">
             <div className="p-0 m-0">
               <Image
                 layout="responsive"
@@ -26,8 +26,8 @@ export const DiscoverBanner: FC<IProps> = (props) => {
                 height={275}
                 src={value.Image}
               />
-              <div className="p-5 pt-4 space-y-2">
-                <h6 className="text-[18px] font-normal text-left w-full">
+              <div className="px-5 pt-4 pb-[70px] space-y-2">
+                <h6 className="text-[16px] md-900:text-[18px] font-normal text-left w-full">
                   {value.Heading}
                 </h6>
                 <h6 className="text-[14px] whitespace-normal leading-[18px] font-sans font-normal text-left w-full opacity-70">
@@ -35,7 +35,7 @@ export const DiscoverBanner: FC<IProps> = (props) => {
                 </h6>
               </div>
             </div>
-            <Button className="text-white mx-5 mt-8 mb-4 p-0 text-[14px] hover:underline underline-offset-4 font-sans font-normal button-text-lower"
+            <Button className="text-white absolute bottom-0 block whitespace-nowrap mx-5 mb-4 p-0 text-[14px] hover:underline underline-offset-4 font-sans font-normal button-text-lower"
               sx={{
                 '.MuiTouchRipple-child': {
                   backgroundColor: 'rgba(225, 225, 255, 0) !important',
