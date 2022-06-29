58ed7ec80bf843854b2cfbf493fd8ad3153d2537
Sumit Paul <sumitpaul16102002@gmail.com>
2022-06-29 21:07:40 +0530

    v1

--- src/components/banner/DiscoverBannerLightingDeal.tsx
+++ src/components/banner/DiscoverBannerLightingDeal.tsx
@@ -3,11 +3,22 @@ import Image from 'next/image';
 import React, { FC } from 'react';
 import useScreenSize from '../../algorithms/ScreenSizeDetection';
 import { DiscoverBannerLightingDealIProps } from '../../contents/store/discover/Store.Discover.Banner';
+import {
+  DiscoverBannerLightningDealDesktop,
+  DiscoverBannerLightningDealMobile,
+  DiscoverBannerLightningDealTablet,
+} from './MultiScreen/DiscoverBannerLightningDeal.MultiScreen';
 
 const HeadingStyle =
   'text-[14px] font-normal text-left whitespace-nowrap overflow-hidden text-ellipsis';
 const DescriptionStyle =
   'text-[13px] whitespace-normal leading-[18px] font-sans font-normal text-left w-full opacity-70 whitespace-nowrap overflow-hidden text-ellipsis';
+const AvailableStyle =
+  'flex w-full h-full p-1.5 font-semibold uppercase tracking-wide text-center text-[11px] flex-col rounded-b-md';
+const ButtonStyle =
+  'p-0 m-0 w-full h-full flex flex-col relative box-border overflow-hidden button-text-lower';
+const ContainerStyle =
+  'p-0 m-0 w-full h-full flex flex-col relative box-border overflow-hidden';
 const OriginalPriceStyle = 'line-through text-[13.5px] opacity-70';
 const DiscountedPriceStyle = 'text-[15px]';
 
@@ -42,176 +53,23 @@ export const DiscoverBannerLightingDeal: FC<IProps> = (props) => {
           </Button>
         </div>
         {LargeScreen ? (
-          <ul className="flex space-x-7 w-full">
-            {props.ContentArray.slice(0, 5).map((value, index) => (
-              <li
-                key={index}
-                className="w-full h-full flex flex-col relative box-border overflow-hidden"
-              >
-                <Image
-                  className="rounded-t-md absolute h-full w-full"
-                  height={320}
-                  width={240}
-                  objectFit="cover"
-                  objectPosition="center"
-                  src={value.Image}
-                  loading="lazy"
-                  alt="Lighting-Deal-Image"
-                />
-                {value.Available ? (
-                  <h6 className="flex w-full h-full p-1.5 font-semibold uppercase tracking-wide text-center text-[11px] flex-col bg-primary-blue-rgb rounded-b-md">
-                    Order Now
-                  </h6>
-                ) : (
-                  <h6 className="flex w-full h-full p-1.5 font-semibold uppercase tracking-wide text-center text-[11px] flex-col bg-[#121212] rounded-b-md">
-                    Sold Out
-                  </h6>
-                )}
-                <div className="mt-6 space-y-1 overflow-hidden">
-                  <h6 className={HeadingStyle}>{value.Heading}</h6>
-                  <h6 className={DescriptionStyle}>{value.Description}</h6>
-                  <div className="text-xs flex items-center space-x-2 pt-1">
-                    <h6
-                      className={OriginalPriceStyle}
-                    >{`₹${value.OriginalPrice}`}</h6>
-                    <h6
-                      className={DiscountedPriceStyle}
-                    >{`₹${value.DiscountedPrice}`}</h6>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <DiscoverBannerLightningDealDesktop
+            ContentArray={props.ContentArray}
+          />
         ) : (
           <></>
         )}
         {MediumScreen ? (
-          <ul className="flex space-x-7 w-full">
-            {props.ContentArray.slice(0, 4).map((value, index) => (
-              <li
-                key={index}
-                className="w-full h-full flex flex-col relative box-border overflow-hidden"
-              >
-                <Image
-                  className="rounded-t-md absolute h-full w-full"
-                  height={320}
-                  width={240}
-                  objectFit="cover"
-                  objectPosition="center"
-                  src={value.Image}
-                  loading="lazy"
-                  alt="Lighting-Deal-Image"
-                />
-                {value.Available ? (
-                  <h6 className="flex w-full h-full p-1.5 font-semibold uppercase tracking-wide text-center text-[11px] flex-col bg-primary-blue-rgb rounded-b-md">
-                    Order Now
-                  </h6>
-                ) : (
-                  <h6 className="flex w-full h-full p-1.5 font-semibold uppercase tracking-wide text-center text-[11px] flex-col bg-[#121212] rounded-b-md">
-                    Sold Out
-                  </h6>
-                )}
-                <div className="mt-6 space-y-1 overflow-hidden">
-                  <h6 className={HeadingStyle}>{value.Heading}</h6>
-                  <h6 className={DescriptionStyle}>{value.Description}</h6>
-                  <div className="text-xs flex items-center space-x-2 pt-1">
-                    <h6
-                      className={OriginalPriceStyle}
-                    >{`₹${value.OriginalPrice}`}</h6>
-                    <h6
-                      className={DiscountedPriceStyle}
-                    >{`₹${value.DiscountedPrice}`}</h6>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <DiscoverBannerLightningDealTablet
+            ContentArray={props.ContentArray}
+          />
         ) : (
           <></>
         )}
         {SmallScreen ? (
-          <div className='w-full relative flex flex-col space-y-7'>
-            <ul className="flex space-x-5 w-full">
-              {props.ContentArray.slice(0, 2).map((value, index) => (
-                <li
-                  key={index}
-                  className="w-full h-full flex flex-col relative box-border overflow-hidden"
-                >
-                  <Image
-                    className="rounded-t-md absolute h-full w-full"
-                    height={307}
-                    width={240}
-                    objectFit="cover"
-                    objectPosition="center"
-                    src={value.Image}
-                    loading="lazy"
-                    alt="Lighting-Deal-Image"
-                  />
-                  {value.Available ? (
-                    <h6 className="flex w-full h-full p-1.5 font-semibold uppercase tracking-wide text-center text-[11px] flex-col bg-primary-blue-rgb rounded-b-md">
-                      Order Now
-                    </h6>
-                  ) : (
-                    <h6 className="flex w-full h-full p-1.5 font-semibold uppercase tracking-wide text-center text-[11px] flex-col bg-[#121212] rounded-b-md">
-                      Sold Out
-                    </h6>
-                  )}
-                  <div className="mt-6 space-y-1 overflow-hidden">
-                    <h6 className={HeadingStyle}>{value.Heading}</h6>
-                    <h6 className={DescriptionStyle}>{value.Description}</h6>
-                    <div className="text-xs flex items-center space-x-2 pt-1">
-                      <h6
-                        className={OriginalPriceStyle}
-                      >{`₹${value.OriginalPrice}`}</h6>
-                      <h6
-                        className={DiscountedPriceStyle}
-                      >{`₹${value.DiscountedPrice}`}</h6>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-            <ul className="flex space-x-5 w-full">
-              {props.ContentArray.slice(2, 4).map((value, index) => (
-                <li
-                  key={index}
-                  className="w-full h-full flex flex-col relative box-border overflow-hidden"
-                >
-                  <Image
-                    className="rounded-t-md absolute h-full w-full"
-                    height={307}
-                    width={240}
-                    objectFit="cover"
-                    objectPosition="center"
-                    src={value.Image}
-                    loading="lazy"
-                    alt="Lighting-Deal-Image"
-                  />
-                  {value.Available ? (
-                    <h6 className="flex w-full h-full p-1.5 font-semibold uppercase tracking-wide text-center text-[11px] flex-col bg-primary-blue-rgb rounded-b-md">
-                      Order Now
-                    </h6>
-                  ) : (
-                    <h6 className="flex w-full h-full p-1.5 font-semibold uppercase tracking-wide text-center text-[11px] flex-col bg-[#121212] rounded-b-md">
-                      Sold Out
-                    </h6>
-                  )}
-                  <div className="my-6 space-y-1 overflow-hidden">
-                    <h6 className={HeadingStyle}>{value.Heading}</h6>
-                    <h6 className={DescriptionStyle}>{value.Description}</h6>
-                    <div className="text-xs flex items-center space-x-2 pt-1">
-                      <h6
-                        className={OriginalPriceStyle}
-                      >{`₹${value.OriginalPrice}`}</h6>
-                      <h6
-                        className={DiscountedPriceStyle}
-                      >{`₹${value.DiscountedPrice}`}</h6>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
+          <DiscoverBannerLightningDealMobile
+            ContentArray={props.ContentArray}
+          />
         ) : (
           <></>
         )}
