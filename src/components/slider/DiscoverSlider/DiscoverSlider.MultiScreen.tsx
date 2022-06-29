21b3837ce4bf4942690d48c7e5722a754a0ab83e
Sumit Paul <sumitpaul16102002@gmail.com>
2022-06-29 20:34:05 +0530

    v1

--- src/components/slider/DiscoverSlider/DiscoverSlider.MultiScreen.tsx
+++ src/components/slider/DiscoverSlider/DiscoverSlider.MultiScreen.tsx
@@ -1,11 +1,28 @@
 import { DiscoverSliderIProps } from '../../../contents/store/discover/Store.Discover.Slider';
-import React, { Dispatch, FC, RefObject, SetStateAction, useEffect } from 'react';
+import React, {
+  Dispatch,
+  FC,
+  RefObject,
+  SetStateAction,
+  useEffect,
+} from 'react';
 import ScrollContainer from 'react-indiana-drag-scroll';
-import { Swiper } from 'swiper/react';
-import {
-  DiscoverSliderDesktopMap,
-  DiscoverSliderMobileMap,
-} from './DiscoverSliderMap';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Button } from '@mui/material';
+import { motion } from 'framer-motion';
+import { Poster_BlurDataURL } from '../../loader/BlurDataURL';
+import Image from 'next/image';
+import { HeartIcon as HeartIconOutline } from '@heroicons/react/outline';
+import { HeartIcon as HeartIconSolid } from '@heroicons/react/solid';
+import { TooltipDark } from '../../tooltip/TooltipDark';
+
+const HeadingStyle =
+  'text-[14px] font-normal text-left w-full whitespace-nowrap overflow-hidden text-ellipsis';
+const DiscountStyle =
+  'bg-primary-blue-rgb font-[600] text-[11px] py-[5px] px-[10px] mr-[2px] rounded-[4px]';
+const OriginalPriceStyle = 'line-through text-[13.5px] opacity-70';
+const DiscountedPriceStyle = 'text-[14px]';
+const ImageStyle = 'rounded-xl';
 
 export interface DiscoverSliderDesktopAndTabletProps {
   ContentArray: DiscoverSliderIProps[];
@@ -59,11 +76,60 @@ export const DiscoverSliderDesktopAndTablet: FC<
         component="ul"
         className="w-full flex px-5 sm:px-0 space-x-4 box-border scroll-smooth"
       >
-        <DiscoverSliderDesktopMap
-          ContentArray={props.ContentArray}
-          Wishlist={props.Wishlist}
-          setWishlist={props.setWishlist}
-        />
+        {props.ContentArray.map((value, index) => (
+          <Button
+            key={index}
+            component="li"
+            disableFocusRipple
+            className="text-white group m-0 p-0 space-y-1 min-w-[197.5px] md-900:min-w-[220px] button-text-lower"
+            sx={{
+              '.MuiTouchRipple-child': {
+                backgroundColor: 'rgba(225, 225, 255, 0.5) !important',
+              },
+            }}
+          >
+            <div className="w-full flex flex-col relative">
+              <div className="relative w-full overflow-hidden">
+                <div className="opacity-0 p-2 flex items-start justify-end group-hover:opacity-100 absolute z-[1] transition-opacity duration-300 rounded-md h-[98%] w-full bg-gradient-to-bl from-[rgba(0,0,0,0.3)]">
+                  <TooltipDark arrow title="Add to Wishlist" placement="top">
+                    <motion.button
+                      onClick={() => props.setWishlist(index)}
+                      whileTap={{ scale: 0.9 }}
+                    >
+                      {props.Wishlist === index ? (
+                        <HeartIconSolid className="h-6 w-6 text-white opacity-100" />
+                      ) : (
+                        <HeartIconOutline className="h-6 w-6 text-white opacity-80" />
+                      )}
+                    </motion.button>
+                  </TooltipDark>
+                </div>
+                <Image
+                  height={307}
+                  width={240}
+                  objectFit="cover"
+                  objectPosition="center"
+                  placeholder="blur"
+                  loading="lazy"
+                  className={ImageStyle}
+                  blurDataURL={Poster_BlurDataURL}
+                  src={value.Image}
+                  alt="product slider image"
+                />
+              </div>
+              <h6 className={HeadingStyle}>{value.Heading}</h6>
+              <div className="text-xs flex items-center space-x-2 pt-1">
+                <h6 className={DiscountStyle}>{value.Discount}</h6>
+                <h6 className={OriginalPriceStyle}>
+                  {`₹${value.OriginalPrice}`}
+                </h6>
+                <h6
+                  className={DiscountedPriceStyle}
+                >{`₹${value.DiscountedPrice}`}</h6>
+              </div>
+            </div>
+          </Button>
+        ))}
       </ScrollContainer>
     </div>
   );
@@ -76,7 +142,7 @@ export interface DiscoverSliderMobileProps {
 }
 export const DiscoverSliderMobile: FC<DiscoverSliderMobileProps> = (props) => {
   return (
-    <div className="w-full h-full flex sm:hidden">
+    <div className="w-full h-full flex">
       <Swiper
         slidesPerView={2}
         spaceBetween={15}
@@ -88,11 +154,66 @@ export const DiscoverSliderMobile: FC<DiscoverSliderMobileProps> = (props) => {
           paddingRight: 20,
         }}
       >
-        <DiscoverSliderMobileMap
-          ContentArray={props.ContentArray}
-          Wishlist={props.Wishlist}
-          setWishlist={props.setWishlist}
-        />
+        {props.ContentArray.map((value, index) => (
+          <SwiperSlide
+            key={index}
+            tag="li"
+            className="flex w-full h-full box-border p-0 m-0"
+          >
+            <Button
+              disableFocusRipple
+              className="text-white group h-full w-full flex m-0 p-0 space-y-1 button-text-lower"
+              sx={{
+                '.MuiTouchRipple-child': {
+                  backgroundColor: 'rgba(225, 225, 255, 0.5) !important',
+                },
+              }}
+            >
+              <div className="w-full flex flex-col">
+                <div className="relative w-full overflow-hidden">
+                  <div className="opacity-0 flex items-start justify-end group-hover:opacity-100 absolute z-[1] transition-opacity duration-300 rounded-md h-[98%] w-full bg-gradient-to-bl from-[rgba(0,0,0,0.3)]">
+                    <motion.button
+                      onClick={() => props.setWishlist(index)}
+                      whileTap={{ scale: 0.9 }}
+                      className="p-2"
+                    >
+                      {props.Wishlist === index ? (
+                        <HeartIconSolid className="h-7 w-7 text-white opacity-100" />
+                      ) : (
+                        <HeartIconOutline className="h-7 w-7 text-white opacity-80" />
+                      )}
+                    </motion.button>
+                  </div>
+                  <Image
+                    height={307}
+                    width={240}
+                    layout="responsive"
+                    objectFit="cover"
+                    objectPosition="center"
+                    placeholder="blur"
+                    loading="lazy"
+                    className={ImageStyle}
+                    blurDataURL={Poster_BlurDataURL}
+                    src={value.Image}
+                    alt="product slider image"
+                  />
+                </div>
+                <h6 className={HeadingStyle}>{value.Heading}</h6>
+                <div className="text-xs flex items-center space-x-1 sm-500:space-x-2 pt-2">
+                  <h6 className={`hidden xs-400:block ${DiscountStyle}`}>
+                    {value.Discount}
+                  </h6>
+                  <h6 className={OriginalPriceStyle}>
+                    {`₹${value.OriginalPrice}`}
+                  </h6>
+                  <h6
+                    className={DiscountedPriceStyle}
+                  >{`₹${value.DiscountedPrice}`}</h6>
+                </div>
+              </div>
+            </Button>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
