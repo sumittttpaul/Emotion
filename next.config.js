c023eb9fe4c2cef4bfcd4259c79413efc5fcf236
SumitPaul16102002 <sumitpaul16102002@gmail.com>
2023-03-04 20:20:01 +0530

    23.H1.1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
+});
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: [
@@ -8,6 +13,4 @@ const nextConfig = {
       'firebasestorage.googleapis.com',
     ],
   },
-};
-
-module.exports = nextConfig;
+});
