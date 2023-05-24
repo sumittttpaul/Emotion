9c6be34993cd583584fa7d45460fc027030a3de7
Sumit Paul <sumitpaul16102002@gmail.com>
2023-05-24 12:19:41 +0530

    23.H1.1

--- src/components/carousel/DiscoverCarousel.tsx
+++ src/components/carousel/DiscoverCarousel.tsx
@@ -12,13 +12,27 @@ interface IProps {
 
 export const DiscoverCarousel: FC<IProps> = (props) => {
   return (
-    <div className="w-full h-[500px] mt-5 flex space-x-2 overflow-hidden">
-      <div className="flex items-center justify-center text-white rounded-r-xl w-[30px] min-w-[30px] bg-[#2E2E2E]"></div>
-      <div className="flex items-center justify-center text-white rounded-xl w-[800px] min-w-[800px] bg-[#2E2E2E]">1</div>
-      <div className="flex items-center justify-center text-white rounded-xl w-[300px] min-w-[300px] bg-[#2E2E2E]">2</div>
-      <div className="flex items-center justify-center text-white rounded-xl w-[300px] min-w-[300px] bg-[#2E2E2E]">3</div>
-      <div className="flex items-center justify-center text-white rounded-xl w-[300px] min-w-[300px] bg-[#2E2E2E]">4</div>
-      <div className="flex items-center justify-center text-white rounded-xl w-[300px] min-w-[300px] bg-[#2E2E2E]">5</div>
+    <div className="w-full h-[500px] mt-5 flex overflow-hidden">
+      <div className="flex w-full h-full space-x-3 px-3 overflow-x-auto scrollbar-hide">
+        <div className="flex items-center justify-center text-white rounded-xl w-[800px] min-w-[800px] bg-[#2E2E2E]">
+          10
+        </div>
+        <div className="flex items-center justify-center text-white rounded-xl w-[800px] min-w-[800px] bg-[#2E2E2E]">
+          1
+        </div>
+        <div className="flex items-center justify-center text-white rounded-xl w-[300px] min-w-[300px] bg-[#2E2E2E]">
+          2
+        </div>
+        <div className="flex items-center justify-center text-white rounded-xl w-[300px] min-w-[300px] bg-[#2E2E2E]">
+          3
+        </div>
+        <div className="flex items-center justify-center text-white rounded-xl w-[300px] min-w-[300px] bg-[#2E2E2E]">
+          4
+        </div>
+        <div className="flex items-center justify-center text-white rounded-xl w-[300px] min-w-[300px] bg-[#2E2E2E]">
+          5
+        </div>
+      </div>
     </div>
   );
 };
