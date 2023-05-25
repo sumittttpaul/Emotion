efa4722155acb61bfabdd23b2e8d9565457a0527
Sumit Paul <sumitpaul16102002@gmail.com>
2023-05-25 20:40:57 +0530

    23.H1.1

--- src/components/carousel/DiscoverCarousel/DiscoverCarousel.Banner.tsx
+++ src/components/carousel/DiscoverCarousel/DiscoverCarousel.Banner.tsx
@@ -13,91 +13,123 @@ interface IProps {
  **/
 
 export const DiscoverCarouselBanner: FC<IProps> = (props) => {
-  const x = useMotionValue(-881);
+  const x = useMotionValue(-3065);
   const animation = useAnimation();
-  const [ActiveIndex, setActiveIndex] = useState(3);
+  const [ActiveIndex, setActiveIndex] = useState(10);
   const [AnimationDuration, setAnimationDuration] = useState(0.4);
   const [DisabledCarousel, setDisabledCarousel] = useState(false);
+  const [CarouselOrder, setCarouselOrder] = useState('left');
 
-  // Assuming 10 Content
-  const ContentsArrayReorder = [
-    ...props.ContentArray.slice(-3),
-    ...props.ContentArray,
-    ...props.ContentArray.slice(0, 8),
-  ];
+  const AnimationValue = (index: number) => {
+    const LeftSpacing = 55;
+    const CarouselSpacing = 12 * index;
+    const CarouselWidth = -300 * index;
+    const ExtraSpacing = index > 0 ? LeftSpacing : 0;
+    const AnimationValue = CarouselWidth - CarouselSpacing + ExtraSpacing;
+    return AnimationValue as number;
+  };
 
   const NextCarouselByIndex = (index: number) => {
     setDisabledCarousel(true);
     setAnimationDuration(0.4);
-    const CarouselSpacing = 12 * index;
-    const CarouselWidth = -300 * index;
-    const ExtraSpacing = index > 0 ? 55 : 0;
-    const animationValue = CarouselWidth - CarouselSpacing + ExtraSpacing;
+    const LeftSpacing = 55;
+    const TotalLeftCarouselValue = -3065 - LeftSpacing;
+    const ZeroIndexCarouselAnimationValue =
+      AnimationValue(index) - TotalLeftCarouselValue - LeftSpacing;
+    const SecondCarouselAnimationValue =
+      AnimationValue(index) - TotalLeftCarouselValue;
+    const FirstCarouselAnimationValue =
+      index < 1
+        ? -ZeroIndexCarouselAnimationValue
+        : AnimationValue(index) + TotalLeftCarouselValue;
+    const CarouselOrderAnimationValue =
+      CarouselOrder === 'left'
+        ? AnimationValue(index)
+        : index > 9
+        ? SecondCarouselAnimationValue
+        : FirstCarouselAnimationValue;
     animation.start({
-      x: animationValue,
+      x: CarouselOrderAnimationValue,
     });
     setActiveIndex(index);
-    console.log(animationValue);
-    console.log(ContentsArrayReorder.length);
+    console.log('Index : ' + index);
+    console.log('Initial Animation value : ' + x.get());
   };
 
   const LoopCarousel = () => {
     setDisabledCarousel(false);
     setAnimationDuration(0);
-    // 2 From Left
-    if (ActiveIndex == 1) {
-      setActiveIndex(11);
-      animation.set({
-        x: -3377,
-      });
+    console.log('Final Animation value : ' + x.get());
+
+    // 5
+    if (ActiveIndex == 4) {
+      setCarouselOrder('right');
+      animation.set({ x: -4313 });
       return;
     }
-    // 3 From Left
-    if (ActiveIndex == 2) {
-      setActiveIndex(12);
-      animation.set({
-        x: -3689,
-      });
-      return;
-    }
-    // 1 from Right
-    if (ActiveIndex == 16) {
-      setActiveIndex(6);
-      animation.set({
-        x: -1817,
-      });
-      return;
-    }
-    // 2 from Right
-    if (ActiveIndex == 15) {
-      setActiveIndex(5);
-      animation.set({
-        x: -1505,
-      });
-      return;
-    }
-    // 3 from Right
     if (ActiveIndex == 14) {
-      setActiveIndex(4);
-      animation.set({
-        x: -1193,
-      });
+      setCarouselOrder('left');
+      animation.set({ x: -4313 });
       return;
     }
-    // 4 from Right
-    if (ActiveIndex == 13) {
-      setActiveIndex(3);
-      animation.set({
-        x: -881,
-      });
+
+    // 6
+    if (ActiveIndex == 5) {
+      setCarouselOrder('left');
+      animation.set({ x: -1505 });
       return;
     }
-    // 5 from Right
-    if (ActiveIndex == 12) {
-      setActiveIndex(2);
-      animation.set({
-        x: -569,
-      });
+    if (ActiveIndex == 15) {
+      setCarouselOrder('right');
+      animation.set({ x: -1505 });
+      return;
+    }
+
+    // 7
+    if (ActiveIndex == 6) {
+      setCarouselOrder('left');
+      animation.set({ x: -1817 });
+      return;
+    }
+    if (ActiveIndex == 16) {
+      setCarouselOrder('right');
+      animation.set({ x: -1817 });
+      return;
+    }
+
+    // 8
+    if (ActiveIndex == 7) {
+      setCarouselOrder('left');
+      animation.set({ x: -2129 });
+      return;
+    }
+    if (ActiveIndex == 17) {
+      setCarouselOrder('right');
+      animation.set({ x: -2129 });
+      return;
+    }
+
+    // 9
+    if (ActiveIndex == 8) {
+      setCarouselOrder('left');
+      animation.set({ x: -2441 });
+      return;
+    }
+    if (ActiveIndex == 18) {
+      setCarouselOrder('right');
+      animation.set({ x: -2441 });
+      return;
+    }
+
+    // 10
+    if (ActiveIndex == 9) {
+      setCarouselOrder('left');
+      animation.set({ x: -2753 });
+      return;
+    }
+    if (ActiveIndex == 19) {
+      setCarouselOrder('right');
+      animation.set({ x: -2753 });
       return;
     }
   };
@@ -105,7 +137,7 @@ export const DiscoverCarouselBanner: FC<IProps> = (props) => {
   return (
     <div className="relative w-full h-[500px] min-h-[500px] flex overflow-hidden">
       <motion.div className="relative box-content h-full">
-        <motion.ul
+        <motion.div
           style={{ x }}
           animate={animation}
           onAnimationComplete={LoopCarousel}
@@ -113,12 +145,13 @@ export const DiscoverCarouselBanner: FC<IProps> = (props) => {
           className="flex w-full h-full space-x-3 px-3"
         >
           <DiscoverCarouselBannerContent
-            ContentArray={ContentsArrayReorder}
+            ContentArray={props.ContentArray}
             onClick={(idx) => NextCarouselByIndex(idx)}
             ActiveIndex={ActiveIndex}
             AnimationDuration={AnimationDuration}
+            CarouselOrder={CarouselOrder}
           />
-        </motion.ul>
+        </motion.div>
       </motion.div>
       {DisabledCarousel && (
         <div className="absolute z-[1] h-full w-full bg-transparent" />
--- src/components/carousel/DiscoverCarousel/DiscoverCarousel.BannerContent.tsx
+++ src/components/carousel/DiscoverCarousel/DiscoverCarousel.BannerContent.tsx
@@ -7,6 +7,7 @@ interface IProps {
   ActiveIndex: number;
   AnimationDuration: number;
   onClick: (idx: number) => void;
+  CarouselOrder: string;
 }
 
 const Variants = {
@@ -26,22 +27,72 @@ const Variants = {
 export const DiscoverCarouselBannerContent: FC<IProps> = (props) => {
   return (
     <>
+      {props.CarouselOrder == 'left' && (
+        <BannerContent
+          CustomIndex={0}
+          Id="Discover-Carousel-Banner-Content-Left"
+          ContentArray={props.ContentArray}
+          ActiveIndex={props.ActiveIndex}
+          AnimationDuration={props.AnimationDuration}
+          onClick={props.onClick}
+        />
+      )}
+      <BannerContent
+        CustomIndex={props.ContentArray.length}
+        Id="Discover-Carousel-Banner-Content-Center"
+        ContentArray={props.ContentArray}
+        ActiveIndex={props.ActiveIndex}
+        AnimationDuration={props.AnimationDuration}
+        onClick={props.onClick}
+      />
+      {props.CarouselOrder == 'right' && (
+        <BannerContent
+          CustomIndex={0}
+          Id="Discover-Carousel-Banner-Content-Right"
+          ContentArray={props.ContentArray}
+          ActiveIndex={props.ActiveIndex}
+          AnimationDuration={props.AnimationDuration}
+          onClick={props.onClick}
+        />
+      )}
+    </>
+  );
+};
+
+interface BannerContentProps {
+  Id: string;
+  CustomIndex: number;
+  ContentArray: DiscoverCarouselContentProps[];
+  ActiveIndex: number;
+  AnimationDuration: number;
+  onClick: (idx: number) => void;
+}
+
+const BannerContent: FC<BannerContentProps> = (props) => {
+  return (
+    <div id={props.Id} className="flex space-x-3">
       {props.ContentArray.map((value, idx) => (
-        <motion.li
-          key={idx}
-          initial={props.ActiveIndex === idx ? 'open' : 'closed'}
-          animate={props.ActiveIndex === idx ? 'open' : 'closed'}
+        <motion.div
+          key={idx + props.CustomIndex}
+          id={`Discover-Carousel-Banner-${idx + props.CustomIndex}`}
+          initial={
+            props.ActiveIndex === idx + props.CustomIndex ? 'open' : 'closed'
+          }
+          animate={
+            props.ActiveIndex === idx + props.CustomIndex ? 'open' : 'closed'
+          }
           variants={Variants}
-          onClick={() => props.onClick(idx)}
+          onClick={() => props.onClick(idx + props.CustomIndex)}
           transition={{ type: 'tween', duration: props.AnimationDuration }}
           className="inline-block rounded-xl w-[300px] min-w-[300px] bg-[#2E2E2E]"
         >
           <div className="flex h-full w-full items-center justify-center text-white">
-            {value.Heading}<br/> 
+            {value.Heading}
+            <br />
             {/* ID : {idx.toString()} */}
           </div>
-        </motion.li>
+        </motion.div>
       ))}
-    </>
+    </div>
   );
 };
