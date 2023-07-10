2ae1ef699d6b2d64ecabab7b78d1a9bcbb15b386
Sumit Paul <sumitpaul16102002@gmail.com>
2023-07-10 19:35:43 +0530

    23.H2.1

--- src/components/ui/SetupUI/Setup.Screen.Content.tsx
+++ src/components/ui/SetupUI/Setup.Screen.Content.tsx
@@ -32,79 +32,74 @@ const SetupLoginOtherAccountScreen = dynamic<SetupLoginOtherAccountScreenProps>(
     import('components/ui/SetupUI/Screen/Login/Setup.Login.OtherAccountScreen')
 );
 
-const SetupLoginOTPScreen = dynamic<SetupLoginOTPScreenProps>(() =>
-  import('components/ui/SetupUI/Screen/Login/Setup.Login.OTPScreen')
-  )
+const SetupLoginOTPScreen = dynamic<SetupLoginOTPScreenProps>(
+  () => import('components/ui/SetupUI/Screen/Login/Setup.Login.OTPScreen')
 );
 
-const SetupLoginPasswordScreen = dynamic<SetupLoginPasswordScreenProps>(() =>
-  import('components/ui/SetupUI/Screen/Login/Setup.Login.PasswordScreen')
-  )
+const SetupLoginPasswordScreen = dynamic<SetupLoginPasswordScreenProps>(
+  () => import('components/ui/SetupUI/Screen/Login/Setup.Login.PasswordScreen')
 );
 
 const SetupLoginForgotPasswordScreen =
-  dynamic<SetupLoginForgotPasswordScreenProps>(() =>
-    import(
-      'components/ui/SetupUI/Screen/Login/Setup.Login.ForgotPasswordScreen'
-    ).then((x) => x.SetupLoginForgotPasswordScreen)
+  dynamic<SetupLoginForgotPasswordScreenProps>(
+    () =>
+      import(
+        'components/ui/SetupUI/Screen/Login/Setup.Login.ForgotPasswordScreen'
+      )
   );
 
-const RegisterNameScreenProps = dynamic<RegisterNameScreenPropsProps>(() =>
-  import(
-    'components/ui/SetupUI/Screen/Register/Setup.Register.NameScreen'
-  ).then((x) => x.RegisterNameScreenProps)
+const SetupRegisterNameScreen = dynamic<SetupRegisterNameScreenProps>(
+  () =>
+    import('components/ui/SetupUI/Screen/Register/Setup.Register.NameScreen')
 );
 
-const RegisterPhoneScreenProps = dynamic<RegisterPhoneScreenPropsProps>(() =>
-  import(
-    'components/ui/SetupUI/Screen/Register/Setup.Register.PhoneScreen'
-  ).then((x) => x.RegisterPhoneScreenProps)
+const SetupRegisterPhoneScreen = dynamic<SetupRegisterPhoneScreenProps>(
+  () =>
+    import('components/ui/SetupUI/Screen/Register/Setup.Register.PhoneScreen')
 );
 
-const RegisterOTPScreenProps = dynamic<RegisterOTPScreenPropsProps>(() =>
-  import('components/ui/SetupUI/Screen/Register/Setup.Register.OTPScreen').then(
-    (x) => x.RegisterOTPScreenProps
-  )
+const SetupRegisterOTPScreen = dynamic<SetupRegisterOTPScreenProps>(
+  () => import('components/ui/SetupUI/Screen/Register/Setup.Register.OTPScreen')
 );
 
-const RegisterEmailScreenProps = dynamic<RegisterEmailScreenPropsProps>(() =>
-  import(
-    'components/ui/SetupUI/Screen/Register/Setup.Register.EmailScreen'
-  ).then((x) => x.RegisterEmailScreenProps)
+const SetupRegisterEmailScreen = dynamic<SetupRegisterEmailScreenProps>(
+  () =>
+    import('components/ui/SetupUI/Screen/Register/Setup.Register.EmailScreen')
 );
 
-const RegisterPasswordScreenProps = dynamic<RegisterPasswordScreenPropsProps>(
+const SetupRegisterPasswordScreen = dynamic<SetupRegisterPasswordScreenProps>(
   () =>
     import(
       'components/ui/SetupUI/Screen/Register/Setup.Register.PasswordScreen'
-    ).then((x) => x.RegisterPasswordScreenProps)
+    )
 );
 
-const RegisterVerifyEmailScreenProps =
-  dynamic<RegisterVerifyEmailScreenPropsProps>(() =>
-    import(
-      'components/ui/SetupUI/Screen/Register/Setup.Register.VerifyEmailScreen'
-    ).then((x) => x.RegisterVerifyEmailScreenProps)
+const SetupRegisterVerifyEmailScreen =
+  dynamic<SetupRegisterVerifyEmailScreenProps>(
+    () =>
+      import(
+        'components/ui/SetupUI/Screen/Register/Setup.Register.VerifyEmailScreen'
+      )
   );
 
-const RegisterProfilePictureScreenProps =
-  dynamic<RegisterProfilePictureScreenPropsProps>(() =>
-    import(
-      'components/ui/SetupUI/Screen/Register/Setup.Register.ProfilePictureScreen'
-    ).then((x) => x.RegisterProfilePictureScreenProps)
+const SetupRegisterProfilePictureScreen =
+  dynamic<SetupRegisterProfilePictureScreenProps>(
+    () =>
+      import(
+        'components/ui/SetupUI/Screen/Register/Setup.Register.ProfilePictureScreen'
+      )
   );
 
-const RegisterBirthdayScreenProps = dynamic<RegisterBirthdayScreenPropsProps>(
+const SetupRegisterBirthdayScreen = dynamic<SetupRegisterBirthdayScreenProps>(
   () =>
     import(
       'components/ui/SetupUI/Screen/Register/Setup.Register.BirthdayScreen'
-    ).then((x) => x.RegisterBirthdayScreenProps)
+    )
 );
 
-const RegisterGenderScreenProps = dynamic<RegisterGenderScreenPropsProps>(() =>
-  import(
-    'components/ui/SetupUI/Screen/Register/Setup.Register.GenderScreen'
-  ).then((x) => x.RegisterGenderScreenProps)
+const SetupRegisterGenderScreen = dynamic<SetupRegisterGenderScreenProps>(
+  () =>
+    import('components/ui/SetupUI/Screen/Register/Setup.Register.GenderScreen')
 );
 
 interface IProps {
@@ -112,7 +107,8 @@ interface IProps {
   AnimationDivClassName?: string;
   Animation: AuthAnimationType;
   userProfile: IUserProfile;
-  CheckInfoHandler: Dispatch<AuthScreenType>;
+  CheckInfoHandlerWithScreen: Dispatch<AuthScreenType>;
+  CheckInfoHandler: VoidType;
 }
 
 function SetupScreenContent({
@@ -121,6 +117,7 @@ function SetupScreenContent({
   Animation,
   userProfile,
   CheckInfoHandler,
+  CheckInfoHandlerWithScreen,
 }: IProps) {
   const { Screen } = SetupHook();
   return (
@@ -145,7 +142,7 @@ function SetupScreenContent({
           ContentClassName={ContentClassName}
           Animation={Animation}
           userProfile={userProfile}
-          CheckInfoHandler={CheckInfoHandler}
+          CheckInfoHandler={CheckInfoHandlerWithScreen}
         />
       )}
       {Screen === 'login-otp' && (
@@ -153,7 +150,7 @@ function SetupScreenContent({
           AnimationDivClassName={AnimationDivClassName}
           ContentClassName={ContentClassName}
           Animation={Animation}
-          CheckInfoHandler={CheckInfoHandler}
+          CheckInfoHandler={CheckInfoHandlerWithScreen}
         />
       )}
       {Screen === 'login-password' && (
@@ -170,142 +167,78 @@ function SetupScreenContent({
           Animation={Animation}
         />
       )}
-      {/* {Screen === 'register-name' && (
-        <RegisterNameScreenProps
-          ClassName={ContentClassName}
+      {Screen === 'register-name' && (
+        <SetupRegisterNameScreen
+          AnimationDivClassName={AnimationDivClassName}
+          ContentClassName={ContentClassName}
           Animation={Animation}
-          setSkipDialog={setSkipDialog}
-          Toast={Toast}
-          Loading={Loading}
-          setAuthScreen={setScreen}
-          setLoading={setLoading}
-          setToast={setToast}
-          setToastSetting={setToastSetting}
-          FullName={FullName}
-          setFullName={setFullName}
-          IsInformation={() => IsInformation_AfterName('normal')}
+          CheckInfoHandler={CheckInfoHandler}
         />
       )}
       {Screen === 'register-phone' && (
-        <RegisterPhoneScreenProps
-          ClassName={ContentClassName}
+        <SetupRegisterPhoneScreen
+          AnimationDivClassName={AnimationDivClassName}
+          ContentClassName={ContentClassName}
           Animation={Animation}
-          PhoneNumber={PhoneNumber}
-          setPhoneNumber={setPhoneNumber}
-          ResetCaptcha={ResetCaptcha}
-          setResetCaptcha={setResetCaptcha}
-          setSkipDialog={setSkipDialog}
-          Toast={Toast}
-          Loading={Loading}
-          setAuthScreen={setScreen}
-          setLoading={setLoading}
-          setToast={setToast}
-          setToastSetting={setToastSetting}
-          IsInformation={() => IsInformation_AfterPhoneAndOTP('normal')}
+          CheckInfoHandler={CheckInfoHandler}
         />
       )}
       {Screen === 'register-otp' && (
-        <RegisterOTPScreenProps
-          ClassName={ContentClassName}
+        <SetupRegisterOTPScreen
+          AnimationDivClassName={AnimationDivClassName}
+          ContentClassName={ContentClassName}
           Animation={Animation}
-          PhoneNumber={PhoneNumber}
-          Toast={Toast}
-          Loading={Loading}
-          setResetCaptcha={setResetCaptcha}
-          setAuthScreen={setScreen}
-          setLoading={setLoading}
-          setToast={setToast}
-          setToastSetting={setToastSetting}
-          IsInformation={() => IsInformation_AfterPhoneAndOTP('normal')}
+          CheckInfoHandler={CheckInfoHandler}
         />
       )}
       {Screen === 'register-email' && (
-        <RegisterEmailScreenProps
-          ClassName={ContentClassName}
+        <SetupRegisterEmailScreen
+          AnimationDivClassName={AnimationDivClassName}
+          ContentClassName={ContentClassName}
           Animation={Animation}
-          setSkipDialog={setSkipDialog}
-          EmailAddress={EmailAddress}
-          setEmailAddress={setEmailAddress}
-          Toast={Toast}
-          Loading={Loading}
-          setAuthScreen={setScreen}
-          setLoading={setLoading}
-          setToast={setToast}
-          setToastSetting={setToastSetting}
-          IsInformation={() => IsInformation_AfterEmailAndPassword('normal')}
+          CheckInfoHandler={CheckInfoHandler}
         />
       )}
       {Screen === 'register-password' && (
-        <RegisterPasswordScreenProps
-          ClassName={ContentClassName}
+        <SetupRegisterPasswordScreen
+          AnimationDivClassName={AnimationDivClassName}
+          ContentClassName={ContentClassName}
           Animation={Animation}
-          EmailAddress={EmailAddress}
-          Toast={Toast}
-          Loading={Loading}
-          setAuthScreen={setScreen}
-          setLoading={setLoading}
-          setToast={setToast}
-          setToastSetting={setToastSetting}
-          IsInformation={() => IsInformation_AfterEmailAndPassword('normal')}
+          CheckInfoHandler={CheckInfoHandler}
         />
       )}
       {Screen === 'register-verify-email' && (
-        <RegisterVerifyEmailScreenProps
-          isEmailVerified={isEmailVerified}
-          ClassName={ContentClassName}
+        <SetupRegisterVerifyEmailScreen
+          AnimationDivClassName={AnimationDivClassName}
+          ContentClassName={ContentClassName}
           Animation={Animation}
-          Toast={Toast}
-          Loading={Loading}
-          setAuthScreen={setScreen}
-          setLoading={setLoading}
-          setToast={setToast}
-          setToastSetting={setToastSetting}
-          IsInformation={() => IsInformation_AfterVerifyEmail('normal')}
+          CheckInfoHandler={CheckInfoHandler}
         />
       )}
       {Screen === 'register-profile-picture' && (
-        <RegisterProfilePictureScreenProps
-          ClassName={ContentClassName}
+        <SetupRegisterProfilePictureScreen
+          AnimationDivClassName={AnimationDivClassName}
+          ContentClassName={ContentClassName}
           Animation={Animation}
-          setSkipDialog={setSkipDialog}
-          Toast={Toast}
-          setAuthScreen={setScreen}
-          setToast={setToast}
-          setToastSetting={setToastSetting}
-          IsInformation={() => IsInformation_AfterProfilePhoto('normal')}
+          CheckInfoHandler={CheckInfoHandler}
         />
       )}
       {Screen === 'register-date-of-birth' && (
-        <RegisterBirthdayScreenProps
-          ClassName={ContentClassName}
+        <SetupRegisterBirthdayScreen
+          AnimationDivClassName={AnimationDivClassName}
+          ContentClassName={ContentClassName}
           Animation={Animation}
-          setSkipDialog={setSkipDialog}
-          Toast={Toast}
-          setLoading={setLoading}
-          setAuthScreen={setScreen}
-          setToast={setToast}
-          setToastSetting={setToastSetting}
-          DateOfBirth={DateOfBirth}
-          setDateOfBirth={setDateOfBirth}
-          IsInformation={() => IsInformation_AfterBirthday('normal')}
+          CheckInfoHandler={CheckInfoHandler}
         />
       )}
       {Screen === 'register-gender' && (
-        <RegisterGenderScreenProps
-          ClassName={ContentClassName}
+        <SetupRegisterGenderScreen
+          AnimationDivClassName={AnimationDivClassName}
+          ContentClassName={ContentClassName}
           Animation={Animation}
-          setSkipDialog={setSkipDialog}
-          Toast={Toast}
-          setFinish={setFinish}
-          setLoading={setLoading}
-          setAuthScreen={setScreen}
-          setToast={setToast}
-          setToastSetting={setToastSetting}
-          Gender={Gender}
-          setGender={setGender}
-          IsInformation={() => IsInformation_AfterGender('normal')}
+          CheckInfoHandler={CheckInfoHandler}
         />
-      )} */}
+      )}
     </AnimatePresence>
   );
 }
